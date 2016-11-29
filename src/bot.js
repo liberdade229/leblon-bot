69edd7a5a30ace0195ec27b32b62f7f736c480c7
Alexandre L. Solleiro <alex@webcracy.org>
2016-11-29 12:14:07 +0000

    Add funny messages, brought to you by ThinkWide

--- src/bot.js
+++ src/bot.js
@@ -4,6 +4,39 @@
 const config = require('./config')
 const Botkit = require('botkit')
 
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1)
+}
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement)
+}
+
+var single_option = [
+  "You must be reading our minds! We’ll put aside a dose of XXX just for you.",
+  "Of course we do have XXX you lucky bastard!",
+  "Yes, we have XXX! And let me say I personally tasted it and it's deliciously good today.",
+  "XXX? Of course we have it. That’s our favourite too!",
+  "Excellent choice! But you need to hurry because XXX is selling out like hot cupcakes.",
+  "Your wish is our command! XXX it is!!"
+]
+
+var no_option = [
+  "Sorry. No XXX for today. We started cooking it but soon realised it would not meet your highest standards.",
+  "Really? XXX? With this weather? You must be kidding...",
+  "No XXX today. Have a salad instead cause summer is just around the corner… Just saying :)",
+  "XXX again?! We cannot have XXX everyday! Try something different for a change.",
+  "Sorry. No XXX for today. We are on a strict diet and so all our customers."
+]
+
+var multi_option = [
+  "Be a little bit more specific, will ya? YYY is not a dish, it’s an ingredient you’ll find on today’s XXX.",
+  "You may find YYY on today’s XXX. Now it’s up to you to choose because I'm just a bot. Not a mind reader.",
+  "There are so many ways to cook YYY. Today you can choose from XXX.",
+  "With YYY you can order XXX. Or be a little crazy and mix them all."
+]
+
 var Menu = []
 
 const commands = {
@@ -25,15 +58,17 @@ const commands = {
     }
 
     var answer = ''
-    if (dishes.length > 0) {
-      answer = 'Good news! You can have '
-      answer += dishes.join(' or ')
-      answer += ' today!'
+    if (dishes.length == 0) {
+      answer = no_option[Math.floor(Math.random() * no_option.length)]
+      answer = answer.replaceAll('XXX', wanted.capitalize())
+    } else if (dishes.length == 1) {
+      answer = single_option[Math.floor(Math.random() * single_option.length)]
+      answer = answer.replaceAll('XXX', dishes[0])
+    } else {
+      answer = multi_option[Math.floor(Math.random() * multi_option.length)]
+      answer = answer.replaceAll('YYY', wanted.capitalize())
+      answer = answer.replaceAll('XXX', dishes.join(' or '))
     }
-    else {
-      answer = 'Bad luck, no '+wanted+' today :disappointed:'
-    }
-
     bot.reply(message, answer)
   }
 }
